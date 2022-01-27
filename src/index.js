8a69c13e8e141cbe162d944660ebe27c3a4f6eab
georgeoikonomitsios <32417846+georgeoikonomitsios@users.noreply.github.com>
2022-01-27 22:15:00 +0300

    Final version of lesson

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name = "Georgios";
+const fname = "Georgios";
+const lname = "Oiko";
 const luckyNumber = 7;
 
 ReactDOM.render(
   <div>
-    <h1>Hello {name}!</h1>
+    <h1>Hello {`${fname} ${lname}`} !</h1>
     <p>Your lucky number is {luckyNumber}</p>
+    <p>Your new lucky number is {Math.floor(Math.random() * 10)}</p>
   </div>,
   document.getElementById("root")
 );
